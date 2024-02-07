2cd030090e4d8b508a06e2c3e3418ed1ac49419d
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-02-07 11:24:08 +0100

    feat: add animation

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const button = document.querySelector('#button');
 const canvas = document.querySelector('#confetti');
+const giftImage = document.querySelector('.gift-image');
 
 const jsConfetti = new JSConfetti();
 
 button.addEventListener('click', () => {
 	jsConfetti.addConfetti().then(() => {});
+  giftImage.style.display = 'block'; // Show the gift image
 });
