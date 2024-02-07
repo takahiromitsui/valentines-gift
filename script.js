979bc849211e85adcc01a67334bcb6cb88767e49
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-02-07 11:34:37 +0100

    feat: change animation

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const giftImage = document.querySelector('.gift-image');
 const jsConfetti = new JSConfetti();
 
 button.addEventListener('click', () => {
-	jsConfetti.addConfetti().then(() => {});
+  jsConfetti.addConfetti().then(() => {});
+  button.textContent = 'You Won!'; // Change the button text
   giftImage.style.display = 'block'; // Show the gift image
 });
+
